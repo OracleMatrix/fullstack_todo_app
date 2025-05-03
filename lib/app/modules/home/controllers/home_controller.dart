dd100b3810af4aa6d94c08f78070b86480baf902
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 21:23:55 +0330

    `Added functionality to HomeController and HomeView to fetch and display user data`

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fullstack_todo_app/app/modules/home/Models/get_user_data_model.dart';
+import 'package:fullstack_todo_app/app/modules/home/providers/get_user_data_provider.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  var isLoading = false.obs;
+
+  var _getUserDataProvider = GetUserDataProvider();
+  get getUserDataProvider => _getUserDataProvider;
+
+  set getUserDataProvider(var value) => _getUserDataProvider = value;
+
+  var userData = GetUserDataModel().obs;
+
+  Future getUserData() async {
+    try {
+      isLoading.value = true;
+      final response = await _getUserDataProvider.getUserData();
+      if (response != null) {
+        userData.value = response;
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
