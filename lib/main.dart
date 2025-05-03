b8c97e41066efac61bca510791018696bae0daba
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 20:33:48 +0330

    `Update dependencies and configuration in main.dart and pubspec.lock`

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.cupertino,
+      title: "TODO",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
