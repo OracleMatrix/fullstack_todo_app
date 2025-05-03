70d5b8b2f686d432c3d74273488e454c84a37669
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 20:50:29 +0330

    `Added authentication functionality to the app, including register and login controllers, providers, and storage key constants.`

--- lib/app/data/Constants/consts.dart
+++ lib/app/data/Constants/consts.dart
@@ -3,4 +3,7 @@ import 'package:get_storage/get_storage.dart';
 class Constants {
   static String baseUrl = 'https://754c-85-203-15-102.ngrok-free.app';
   static GetStorage storage = GetStorage();
+  static String userIdKey = 'userId';
+  static String userNameKey = 'userName';
+  static String tokenKey = 'token';
 }
--- lib/app/modules/Authentication/controllers/authentication_controller.dart
+++ lib/app/modules/Authentication/controllers/authentication_controller.dart
@@ -1,3 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:fullstack_todo_app/app/data/Constants/consts.dart';
+import 'package:fullstack_todo_app/app/modules/Authentication/providers/login_provider.dart';
+import 'package:fullstack_todo_app/app/modules/Authentication/providers/register_provider.dart';
+import 'package:fullstack_todo_app/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
-class AuthenticationController extends GetxController {}
+class AuthenticationController extends GetxController {
+  var _registerProvider = RegisterProvider();
+  get registerProvider => _registerProvider;
+
+  set registerProvider(var value) => _registerProvider = value;
+
+  var _loginProvider = LoginProvider();
+  get loginProvider => _loginProvider;
+
+  set loginProvider(var value) => _loginProvider = value;
+
+  var isLoading = false.obs;
+
+  Future register(String username, String email, String password) async {
+    try {
+      isLoading.value = true;
+      final data = {'username': username, 'email': email, 'password': password};
+      final response = await _registerProvider.register(data);
+      if (response != null) {
+        Constants.storage.write(Constants.userIdKey, response['user']['id']);
+        Constants.storage.write(
+          Constants.userIdKey,
+          response['user']['username'],
+        );
+        Constants.storage.write(Constants.userIdKey, response['token']);
+        Get.offAllNamed(Routes.HOME);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future login(String username, String password) async {
+    try {
+      isLoading.value = true;
+      final data = {'username': username, 'password': password};
+      final response = await _loginProvider.login(data);
+      if (response != null) {
+        Constants.storage.write(Constants.userIdKey, response['user']['id']);
+        Constants.storage.write(
+          Constants.userIdKey,
+          response['user']['username'],
+        );
+        Constants.storage.write(Constants.userIdKey, response['token']);
+        Get.offAllNamed(Routes.HOME);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
