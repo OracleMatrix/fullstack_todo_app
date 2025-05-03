14b218b5a235eb268d5b900d1ae8ffccd68c597e
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 21:31:34 +0330

    `Added loading animation and empty state handling to HomeView`

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,11 +9,52 @@ class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
+    controller.getUserData();
     return Scaffold(
       appBar: AppBar(title: const Text('T O D O'), centerTitle: true),
-      body: const Center(
-        child: Text('HomeView is working', style: TextStyle(fontSize: 20)),
-      ),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return Center(
+            child: LoadingAnimationWidget.dotsTriangle(
+              color: Colors.blue,
+              size: 80,
+            ),
+          );
+        }
+        if (controller.userData.value.todos?.isEmpty ?? [].isNotEmpty) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.hourglass_empty, size: 100),
+                ),
+                Text(
+                  'You have no TODOs yet...!',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          );
+        }
+        return ListView.builder(
+          itemCount: controller.userData.value.todos!.length,
+          itemBuilder: (context, index) {
+            final todo = controller.userData.value.todos![index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(todo.title ?? 'No title!'),
+                  subtitle: Text(todo.description ?? 'No description!'),
+                  leading: Icon(Icons.task, color: Colors.blue),
+                ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
