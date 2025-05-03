b6b7b5b6a7a57583dd6f7e8eb120441abd2bbee3
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 21:20:21 +0330

    `Added getUserData method to GetUserDataProvider and updated imports`

--- lib/app/modules/home/providers/get_user_data_provider.dart
+++ lib/app/modules/home/providers/get_user_data_provider.dart
@@ -1,8 +1,33 @@
+import 'package:fullstack_todo_app/app/data/Constants/consts.dart';
+import 'package:fullstack_todo_app/app/modules/home/Models/get_user_data_model.dart';
 import 'package:get/get.dart';
 
 class GetUserDataProvider extends GetConnect {
-  @override
-  void onInit() {
-    httpClient.baseUrl = 'YOUR-API-URL';
+  Future<GetUserDataModel?> getUserData() async {
+    try {
+      final response = await get(
+        '${Constants.baseUrl}/api/users/${await Constants.storage.read(Constants.userIdKey)}',
+        headers: {
+          'authentication': await Constants.storage.read(Constants.tokenKey),
+        },
+      );
+      if (response.body != null) {
+        if (response.status.isOk) {
+          return getUserDataModelFromJson(response.bodyString!);
+        } else if (response.status.isServerError) {
+          throw 'Server is not reachable\nPlease try again later';
+        } else if (response.status.isNotFound) {
+          throw 'Not Found!';
+        } else if (response.status.isForbidden) {
+          throw 'Bad request\nCheck data and try again...';
+        } else {
+          throw response.body['message'] ?? response.bodyString;
+        }
+      } else {
+        throw 'Something went wrong!\nPlease try again later';
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 }
