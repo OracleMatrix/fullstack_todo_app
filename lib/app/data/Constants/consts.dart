3b4db317a203ef64d723abbc865a460eb2f0ed17
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-03 20:42:07 +0330

    `feat: Update login and register providers to use Constants.baseUrl`

---
+++ lib/app/data/Constants/consts.dart
@@ -0,0 +1,6 @@
+import 'package:get_storage/get_storage.dart';
+
+class Constants {
+  static String baseUrl = 'https://754c-85-203-15-102.ngrok-free.app';
+  static GetStorage storage = GetStorage();
+}
--- lib/app/modules/Authentication/providers/login_provider.dart
+++ lib/app/modules/Authentication/providers/login_provider.dart
@@ -1,8 +1,27 @@
+import 'package:fullstack_todo_app/app/data/Constants/consts.dart';
 import 'package:get/get.dart';
 
 class LoginProvider extends GetConnect {
-  @override
-  void onInit() {
-    httpClient.baseUrl = 'YOUR-API-URL';
+  Future login(Map<String, dynamic> data) async {
+    try {
+      final response = await post('${Constants.baseUrl}/api/auth/login', data);
+      if (response.body != null) {
+        if (response.status.isOk) {
+          return response.body;
+        } else if (response.status.isServerError) {
+          throw 'Server is not reachable\nPlease try again later';
+        } else if (response.status.isNotFound) {
+          throw 'Not Found!';
+        } else if (response.status.isForbidden) {
+          throw 'Bad request\nCheck data and try again...';
+        } else {
+          throw response.body['message'] ?? response.bodyString;
+        }
+      } else {
+        throw 'Something went wrong!\nPlease try again later';
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 }
