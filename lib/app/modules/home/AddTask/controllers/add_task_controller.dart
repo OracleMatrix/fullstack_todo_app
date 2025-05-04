de53564f416f14b6e43369026f10ff759c5b6e78
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-04 20:23:39 +0330

    `Added functionality to AddTaskController: loading indicator, providers, and task creation logic`

--- lib/app/modules/home/AddTask/controllers/add_task_controller.dart
+++ lib/app/modules/home/AddTask/controllers/add_task_controller.dart
@@ -1,23 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:fullstack_todo_app/app/modules/home/AddTask/providers/add_task_provider.dart';
 import 'package:get/get.dart';
 
 class AddTaskController extends GetxController {
-  //TODO: Implement AddTaskController
+  var isLoading = false.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  var _addTaskProvider = AddTaskProvider();
+  get addTaskProvider => _addTaskProvider;
+
+  set addTaskProvider(var value) => _addTaskProvider = value;
+
+  final priority = 'low'.obs;
+
+  var titleController = TextEditingController().obs;
+  var descriptionController = TextEditingController().obs;
+
+  Future addTask() async {
+    try {
+      isLoading.value = true;
+      final data = {
+        'title': titleController.value.text,
+        'description': descriptionController.value.text,
+        'priority': priority.value,
+      };
+      final response = await _addTaskProvider.addTask(data);
+      if (response != null) {
+        Get.back();
+        Get.snackbar(
+          'Success',
+          response['message'],
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+          icon: Icon(Icons.check, color: Colors.white),
+        );
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
