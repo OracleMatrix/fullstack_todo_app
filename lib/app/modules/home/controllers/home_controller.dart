91677f2c05ccb05a9d20aa0fdc75c018b7fedd78
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-04 21:19:57 +0330

    `Added MarkAsDone provider and markAsCompleted method to HomeController`

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fullstack_todo_app/app/modules/home/Models/get_user_data_model.dart';
 import 'package:fullstack_todo_app/app/modules/home/providers/get_user_data_provider.dart';
+import 'package:fullstack_todo_app/app/modules/home/providers/mark_as_done_provider.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -13,12 +14,44 @@ class HomeController extends GetxController {
 
   var userData = GetUserDataModel().obs;
 
+  var _markAsCompletedProvider = MarkAsDoneProvider();
+  get markAsCompletedProvider => _markAsCompletedProvider;
+
+  set markAsCompletedProvider(var value) => _markAsCompletedProvider = value;
+
   Future getUserData() async {
     try {
       isLoading.value = true;
       final response = await _getUserDataProvider.getUserData();
       if (response != null) {
         userData.value = response;
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future markAsCompleted(int todoId) async {
+    try {
+      isLoading.value = true;
+      final response = await _markAsCompletedProvider.markAsCompleted(todoId);
+      if (response != null) {
+        Get.snackbar(
+          'Success',
+          response['message'],
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+          icon: Icon(Icons.check, color: Colors.white),
+        );
+        await getUserData();
       }
     } catch (e) {
       Get.snackbar(
