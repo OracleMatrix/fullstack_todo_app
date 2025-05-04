4c68b868886c1fe2b70c4f5185c5cac729b4e2db
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-04 14:40:39 +0330

    `Update Constants/consts.dart with new ngrok URL`

--- lib/app/data/Constants/consts.dart
+++ lib/app/data/Constants/consts.dart
@@ -1,7 +1,7 @@
 import 'package:get_storage/get_storage.dart';
 
 class Constants {
-  static String baseUrl = 'https://20e5-85-203-15-168.ngrok-free.app';
+  static String baseUrl = 'https://9f43-85-203-15-175.ngrok-free.app';
   static GetStorage storage = GetStorage();
   static String userIdKey = 'userId';
   static String userNameKey = 'userName';
