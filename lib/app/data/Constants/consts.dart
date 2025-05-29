4f468797cb281c9fb1ce477252b868682498f8ae
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-29 12:23:55 +0330

    Here is the generated commit message:
    `chore(consts): update base URL to localhost`

--- lib/app/data/Constants/consts.dart
+++ lib/app/data/Constants/consts.dart
@@ -1,7 +1,7 @@
 import 'package:get_storage/get_storage.dart';
 
 class Constants {
-  static String baseUrl = 'https://e093-85-203-15-232.ngrok-free.app';
+  static String baseUrl = 'http://localhost:3000';
   static GetStorage storage = GetStorage();
   static String userIdKey = 'userId';
   static String userNameKey = 'userName';
