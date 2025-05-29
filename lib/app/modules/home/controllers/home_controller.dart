a7480c0aa3d1c7425f1a685d512c4179810d0c51
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-29 12:08:09 +0330

    feat(home): add search functionality to home controller
    Added search functionality to the home controller, including a search provider, search model, and search logic. The search function is triggered when the user types in the search bar, and the results are displayed in the todosData list.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:fullstack_todo_app/app/modules/home/Models/get_todos_model.dart';
 import 'package:fullstack_todo_app/app/modules/home/Models/get_user_data_model.dart';
 import 'package:fullstack_todo_app/app/modules/home/providers/get_user_data_provider.dart';
 import 'package:fullstack_todo_app/app/modules/home/providers/mark_as_done_provider.dart';
+import 'package:fullstack_todo_app/app/modules/home/providers/search_provider.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  // VARIABLES
   var isLoading = false.obs;
+  var isSearching = false.obs;
 
+  var searchController = TextEditingController().obs;
+
+  // PROVIDERS
   var _getUserDataProvider = GetUserDataProvider();
+
   get getUserDataProvider => _getUserDataProvider;
 
   set getUserDataProvider(var value) => _getUserDataProvider = value;
 
-  var userData = GetUserDataModel().obs;
-
   var _markAsCompletedProvider = MarkAsDoneProvider();
+
   get markAsCompletedProvider => _markAsCompletedProvider;
 
   set markAsCompletedProvider(var value) => _markAsCompletedProvider = value;
 
+  var _searchTodosProvider = SearchProvider();
+
+  get searchTodosProvider => _searchTodosProvider;
+
+  set searchTodosProvider(value) {
+    _searchTodosProvider = value;
+  }
+
+  // MODELS
+  var userData = GetUserDataModel().obs;
+  var todosData = <GetTodosModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    searchController.value.addListener(() {
+      if (searchController.value.text.isEmpty && isSearching.value) {
+        isSearching.value = false;
+        todosData.clear();
+      }
+    });
+  }
+
+  // FUNCTIONS
   Future getUserData() async {
     try {
       isLoading.value = true;
@@ -52,6 +83,35 @@ class HomeController extends GetxController {
           icon: Icon(Icons.check, color: Colors.white),
         );
         await getUserData();
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future searchTodos() async {
+    final query = searchController.value.text.trim();
+    if (query.isEmpty) {
+      // Clear search
+      isSearching.value = false;
+      todosData.clear();
+      return;
+    }
+
+    try {
+      isLoading.value = true;
+      final response = await _searchTodosProvider.searchTodos(query);
+      if (response != null) {
+        todosData.assignAll(response);
+        isSearching.value = true;
       }
     } catch (e) {
       Get.snackbar(
